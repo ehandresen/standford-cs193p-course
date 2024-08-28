b7068a26ea1603ab498c30c2cb3a2470ca7b153e
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-08-28 20:21:21 +0200

    first commit

---
+++ ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  card-game
+//
+//  Created by Eirik Andresen on 28/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack(content: {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
+        })
+        .padding()
+        .foregroundColor(.orange)
+    }
+}
+
+struct CardView: View {
+    // all structs with a var's need a value
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ card_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  card_gameApp.swift
+//  card-game
+//
+//  Created by Eirik Andresen on 28/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
