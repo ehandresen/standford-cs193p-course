d1550c454ab031a299f669a7a99bc2e90f145f1e
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-10-16 22:42:47 +0200

    clean up code, remove button to add / remove cards

--- ContentView.swift
+++ ContentView.swift
@@ -10,16 +10,9 @@ import SwiftUI
 struct ContentView: View {
     let emojis = ["👻", "🎃", "🕷️", "😈", "👽", "💀", "🧙‍♀️", "🧟‍♀️", "🕸️", "🧛‍♂️", "☠️"]
     
-    // can't change cardCount without adding '@State'
-    @State var cardCount: Int = 4
-    
     var body: some View {
-        VStack {
-            ScrollView {
+        ScrollView {
                 cards
-            }
-            Spacer()
-            cardCountAdjusters
         }
         .padding()
     }
@@ -27,46 +20,15 @@ struct ContentView: View {
     var cards: some View {
         // implicit return if its only one line of code
         // this code creates an LazyVGrid and then modifies it
-        // LazyVGrid uses as LITTLE space as it can, HStac uses a MUCH space as it can
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 150))]) {
+        // LazyVGrid uses as LITTLE space as it can, HStack uses a MUCH space as it can
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85))]) {
             // the ForEach is saying: give me the view u want for each of theese things
-            ForEach(0..<cardCount, id: \.self) { index in
+            ForEach(emojis.indices, id: \.self) { index in
                 CardView(content: emojis[index])
                     .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
-    }
-    
-    var cardCountAdjusters: some View {
-        HStack {
-            cardRemover
-            Spacer()
-            cardAdder
-        }
-        .imageScale(.large)
-        .font(.largeTitle)
-    }
-    
-    // 'by' is the external parameter name used when calling the function
-    // 'offset' is the internal parameter name used inside the function's body
-    // 'symbol' is used as both the external and internal parameter name
-    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
-        Button(action: {
-            cardCount += offset
-        }, label: {
-            Image(systemName: symbol)
-        })
-        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
-    }
-    
-    var cardRemover: some View {
-        // again implicit return becuase one line of code so return is unessassery
-        return cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus")
-    }
-    
-    var cardAdder: some View {
-        cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus")
     }
 }
 
